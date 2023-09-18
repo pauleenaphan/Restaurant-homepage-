091239ba542618a9d73562c0e396c4f5f94be3ef
pauleena phan <pauleena2002@gmail.com>
2023-09-17 22:57:24 -0700

    created index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import {content} from "/page.js"
+import {home} from '../pages/home.js'
+//We need double dots because it thinks we are in the src directory 
+//double dots will tell them to look for a new parent directory(pages) and therefore look for home.js
+
+
+home();
