d3b7360092bb4ec2d02e4e4c0f7aa5cfadbe6cb4
pauleena phan <pauleena2002@gmail.com>
2023-09-27 23:22:46 -0700

    created menu page

---
+++ pages/menu.js
@@ -0,0 +1,10 @@
+export function menu(){
+    const content = document.getElementById("content");
+
+    const menu = document.createElement("div");
+    menu.classList.add("menupage");
+    content.append(menu);
+
+    
+
+}
