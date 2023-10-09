f2e2b6399676b9237fe5370fd9235e9fd40cb6aa
pauleena phan <pauleena2002@gmail.com>
2023-10-08 20:44:49 -0700

    finished page eventlistener

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {home} from '../pages/home.js'
 import {menu} from '../pages/menu.js'
+import {contact} from '../pages/contact.js'
 //We need double dots because it thinks we are in the src directory 
 //double dots will tell them to look for a new parent directory(pages) and therefore look for home.js
 
@@ -39,6 +40,12 @@ function displayTab(e){
         for(let i = 0; i < tabs.length; i++){
             tabs[i].addEventListener('click', displayTab);
         }    
+    }else if(val == "Contact Us"){
+        content.children[0].remove();
+        contact();
+        for(let i = 0; i < tabs.length; i++){
+            tabs[i].addEventListener('click', displayTab);
+        }  
     }
     //depending on the tab pressed, load the page
 }
