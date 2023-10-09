f0a4ff08b477cd7f3f1d4470c8dfc6b1cb4d66c1
pauleena phan <pauleena2002@gmail.com>
2023-10-08 20:43:55 -0700

    finished menu page

--- pages/menu.js
+++ pages/menu.js
@@ -29,9 +29,46 @@ export function menu(){
     contact_tab.textContent = "Contact Us";
     tabs.append(contact_tab);
 
-    const dish_title = document.createElement("div");
-    dish_title.classname = "dish_title";
-    dish_title.textContent = "BIg Bean Stack";
-    menu.append(dish_title);
+    const dishes = document.createElement("div");
+    dishes.className = "dishes";
+    menu.append(dishes);
 
+    const d1 = document.createElement("div");
+    d1.className = "dish_title";
+    d1.textContent = "Big Bean Stack";
+    dishes.append(d1);
+
+    const p1 = document.createElement("img");
+    p1.className = "dish_pic";
+    p1.src = '../img/bigbeanstack.jpg'
+    dishes.append(p1);
+    
+    const d2 = document.createElement("div");
+    d2.className = "dish_title";
+    d2.textContent = "Bean Fries";
+    dishes.append(d2);
+
+    const p2 = document.createElement("img");
+    p2.className = "dish_pic";
+    p2.src = "../img/beanfries.jpg";
+    dishes.append(p2);
+
+    const d3 = document.createElement("div");
+    d3.className = "dish_title";
+    d3.textContent = "Water";
+    dishes.append(d3);
+
+    const p3 = document.createElement("img");
+    p3.className = "dish_pic";
+    p3.src = "../img/water.jpg";
+    dishes.append(p3);
+
+    const footer = document.createElement("id");
+    footer.id = "footer";
+    footer.textContent = "Copyright @ 2023 Pauleena Phan";
+    menu.append(footer);
+
+    // const footerimg = document.createElement("img");
+    // footerimg.src = "../img/githubicon.png";
+    // menu.append(footerimg);
 }
